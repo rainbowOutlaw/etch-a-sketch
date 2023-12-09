0f52bc2e545c1253ff2a5876f8e87aa3ebdc1f02
saswat-pattnaik <148523561+saswat-pattnaik@users.noreply.github.com>
2023-12-09 19:39:35 +0530

    feat: add required input funcnality to control boxes

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 const container = document.querySelector('.container');
 const input = document.querySelector('#size');
 const inputVal = document.querySelector('.num-boxes');
+let numSide = input.value;
 
-const size = 500/30;
+function createBoard(){
+    const size = 500/numSide;
 
-const boxes = Math.floor((500 ** 2)/(size*size));
+    const boxes = Math.floor((500 ** 2)/(size*size));
 
-function addBox(){
-    container.insertAdjacentHTML(
-        'beforeend',
-        `<div class="box"></div>`      
-    )
-}
+    function addBox(){
+        container.insertAdjacentHTML(
+            'beforeend',
+            `<div class="box"></div>`      
+        )
+    }
 
-for(let i=0; i<boxes; i++){
-    addBox();
-}
+    for(let i=0; i<boxes; i++){
+        addBox();
+    }
+    const allBoxes = document.querySelectorAll('.box');
 
-const allBoxes = document.querySelectorAll('.box');
+    const boxesArr = [...allBoxes];
 
-const boxesArr = [...allBoxes];
-
-boxesArr.map((boxElem) => {
-    boxElem.style['flex-basis'] = `${size}px`;
-    boxElem.style.height = `${size}px`; 
-    boxElem.addEventListener('mouseleave', () => {
-        boxElem.style['background-color'] = 'pink';
+    boxesArr.map((boxElem) => {
+        boxElem.style['flex-basis'] = `${size}px`;
+        boxElem.style.height = `${size}px`; 
+        boxElem.style['background-color'] = 'black';
+        boxElem.addEventListener('mouseenter', () => {
+            boxElem.style['background-color'] = 'pink';
+        })
     })
-})
+}
+
+inputVal.textContent = numSide;
+
+createBoard();
 
 // Range-input change
 
 input.addEventListener('input', (event) => {
-    inputVal.textContent = event.target.value;
+    numSide = event.target.value
+    input.value = numSide;
+    inputVal.textContent = numSide;
+    createBoard();
 })
