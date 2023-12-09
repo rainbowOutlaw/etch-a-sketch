fbdd0ab211cf56ce7be0729c1265866ff1a72062
saswat-pattnaik <148523561+saswat-pattnaik@users.noreply.github.com>
2023-12-09 12:35:11 +0530

    feat: add logic for 16 by 16 squares and complete board

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+
+const size = 500/30;
+
+const boxes = Math.floor((500 ** 2)/(size*size));
+
+function addBox(){
+    container.insertAdjacentHTML(
+        'beforeend',
+        `<div class="box"></div>`      
+    )
+}
+
+for(let i=0; i<boxes; i++){
+    addBox();
+}
+
+const allBoxes = document.querySelectorAll('.box');
+
+const boxesArr = [...allBoxes];
+
+boxesArr.map((boxElem) => {
+    boxElem.style['flex-basis'] = `${size}px`;
+    boxElem.style.height = `${size}px`; 
+    boxElem.addEventListener('mouseleave', () => {
+        boxElem.style['background-color'] = 'pink';
+    })
+})
